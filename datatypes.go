e01372194c0320052733a63ce822a961c0817c6d
landru27 <landru729@gmail.com>
2017-08-06 02:16:35 -0400

    initial attempt to add TileTicks around the newly built structures, as a further attempt to start off with correct lighting; this code builds up a collection of TileTick data objects while running, but that data isn't being written out to the destination region file along with all the other chunkdata; so further diagnostics and fixes are warranted

--- datatypes.go
+++ datatypes.go
@@ -124,6 +124,42 @@ func (w *MCWorld) EditBlock(x int, y int, z int, id uint16, data uint8) (err err
 	dataLightPopulated := rgn.Chunks[indxChunk].ChunkDataRefs["LightPopulated"]
 	dataLightPopulated.Data = byte(0)
 
+	if id == 0 {
+		if _, okay := rgn.Chunks[indxChunk].ChunkDataRefs["TileTicks"]; !okay {
+			dataLevel := rgn.Chunks[indxChunk].ChunkDataRefs["Level"]
+
+			nbtTicks := NBT{TAG_List, TAG_Compound, "TileTicks", 0, make([]NBT, 0)}
+
+			dataLevel.Size++
+			dataLevel.Data = append(dataLevel.Data.([]NBT), nbtTicks)
+
+			rgn.Chunks[indxChunk].ChunkDataRefs["TileTicks"] = &dataLevel.Data.([]NBT)[dataLevel.Size - 1]
+		}
+
+		var strid string
+		switch id {
+		case 0:
+			strid = "minecraft:air"
+		default:
+			strid = "minecraft:air"
+		}
+
+		nbtI := NBT{TAG_String, 0, "i", uint32(len(strid)), strid}
+		nbtT := NBT{TAG_Int, 0, "t", 0, 20}
+		nbtP := NBT{TAG_Int, 0, "p", 0, 0}
+		nbtX := NBT{TAG_Int, 0, "x", 0, x}
+		nbtY := NBT{TAG_Int, 0, "y", 0, y}
+		nbtZ := NBT{TAG_Int, 0, "z", 0, z}
+		nbts := []NBT{nbtI, nbtT, nbtP, nbtX, nbtY, nbtZ}
+
+		nbtTick := NBT{TAG_Compound, 0, "LISTELEM", 6, nbts}
+
+		dataTileTicks := rgn.Chunks[indxChunk].ChunkDataRefs["TileTicks"]
+		dataTileTicks.List = TAG_Compound
+		dataTileTicks.Size++;
+		dataTileTicks.Data = append(dataTileTicks.Data.([]NBT), nbtTick)
+	}
+
 	qtyBlockEdits++
 
 	return
@@ -541,6 +577,8 @@ func (c *MCChunk) BuildDataRefs() {
 	c.ChunkDataRefs = make(map[string]*NBT, 0)
 
 	refLevl := c.ChunkData.Data.([]NBT)[0]
+
+	c.ChunkDataRefs["Level"] = &refLevl
 
 	for indxA, elemLevl := range refLevl.Data.([]NBT) {
 		c.ChunkDataRefs[elemLevl.Name] = &refLevl.Data.([]NBT)[indxA]
